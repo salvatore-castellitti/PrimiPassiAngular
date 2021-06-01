ac0ec1a4cd3a5179d22a3381fbeb98d221a6658e
Salvatore <salvatore.castellitti@si2001.it>
2021-06-01 15:12:44 +0200

    Table Searching Filter

---
+++ src/app/amount-pages-table.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { AmountPagesTablePipe } from './amount-pages-table.pipe';
+
+describe('AmountPagesTablePipe', () => {
+  it('create an instance', () => {
+    const pipe = new AmountPagesTablePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
